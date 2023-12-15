5de5603f681f7faebdd0e9abd16ccb7e14715ec5
tati <miranda.tatianayamila@gmail.com>
2023-12-15 16:21:06 +0100

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
+import { NavComponent } from './layouts/nav/nav.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { HomeComponent } from './component/home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+     NavComponent,
+     FooterComponent,
+     HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layouts/nav/nav.component.ts
+++ src/app/layouts/nav/nav.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.sass']
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
 
